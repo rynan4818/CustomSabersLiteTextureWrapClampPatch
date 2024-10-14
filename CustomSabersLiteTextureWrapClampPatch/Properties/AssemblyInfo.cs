d8abc87db9e9465347982830829ebac0cbe305e4
rynan4818 <14249877+rynan4818@users.noreply.github.com>
2024-10-14 14:28:31 +0900

    初回リリース

--- CustomSabersLiteTextureWrapClampPatch/Properties/AssemblyInfo.cs
+++ CustomSabersLiteTextureWrapClampPatch/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CustomSabersLiteTextureWrapClampPatch")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyCopyright("Copyright ©  2024 Rynan")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
